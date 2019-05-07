04e02bfcab3916137d45567df660950f0dd377e2
Brian Peck <brian.peck@thecontrolgroup.com>
2019-05-07 03:37:35 -0700

    Added command line arguments

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,16 +1,34 @@
 #!usr/bin/env python
 
+# Optperse allows options in cli
+import optparse
+
+# Scapy allows packet sniffing
 try:
     import scapy.all as scapy
 except ImportError:
     import scapy
 
+# Scapy HTTP allows http packet support
 try:
     # This import works from the project directory
     from scapy_http import http
 except ImportError:
     # If you installed this package via pip, you just need to execute this
     from scapy.layers import http
+
+# Create command line arguments to be consumed by program
+def c_args():
+	# Calls the optparse package
+	parser = optparse.OptionParser()
+	# Sets allowable command line parameters and --help descriptions
+	parser.add_option("-i", "--interface", dest="interface", help="Network Interface Card to sniff packets on")
+	
+	# Gather arguments and they're values (options)
+	(options, _) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Please specify an interface. Use --help for more info")
+	return options
 
 def sniff(interface):
     # interface, the interface being sniffed, store, whether to store packets in memory (takes toll on cpu),
@@ -25,4 +43,7 @@ def process_sniffed_packet(packet):
         print("----------END PACKET---------/")
         print("/----------------------------/")
 
-sniff("en0")
+# Create the initiating arguments
+options = c_args()
+
+sniff(options.interface)
